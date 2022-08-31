5d8141fb0757096209ea8cbc2eb48fa6e31bcdb7
Vishwas <vishwasanand.cse@gmail.com>
2022-08-31 12:11:54 +0530

    updated hsconstants

--- src/popup/utils/hsConstants.js
+++ src/popup/utils/hsConstants.js
@@ -1,11 +1,10 @@
-export const HS_NODE_BASE_URL = process.env.VUE_APP_HS_NODE_BASE_URL || "https://ssi.hypermine.in/core/"; 
-export const SUPERHERO_HS_AUTH_BASE_URL = process.env.VUE_APP_HS_AUTH_BASE_URL || "https://ssi.hypermine.in/hsauth/";
+export const HS_NODE_BASE_URL = process.env.VUE_APP_HS_NODE_BASE_URL || "https://api.fyre.hypersign.id/core/";
+export const SUPERHERO_HS_AUTH_BASE_URL = process.env.VUE_APP_HS_AUTH_BASE_URL || "https://api.fyre.hypersign.id/hsauth/";
 export const SUPERHERO_HS_AUTH_CREDENTIAL_ISSUE_API = "hs/api/v2/register"; // change supero to hypersign later
 export const HS_AUTH_DID_URL = SUPERHERO_HS_AUTH_BASE_URL + "hs/api/v2/authdid"
 export const WALLET_TYPE = process.env.VUE_APP_HS_WALLET_TYPE || "web"
 export const HYPERSIGN_AUTH_PROVIDER = {
-    GOOGLE : 'google',
-    TWITTER : 'twitter',
-    FACEBOOK : 'facebook'
+    GOOGLE: 'google',
+    TWITTER: 'twitter',
+    FACEBOOK: 'facebook'
 }
-                                          
