c67ce9beeb351d19ed1a4f406e9ac8ae1b7a3bd8
vishwas1 <vishwasanand.cse@gmail.com>
2022-09-02 19:41:55 +0530

    updated the right link

--- src/popup/utils/hsConstants.js
+++ src/popup/utils/hsConstants.js
@@ -1,5 +1,5 @@
-export const HS_NODE_BASE_URL = process.env.VUE_APP_HS_NODE_BASE_URL || "https://stage.hypermine.in/core/";
-export const SUPERHERO_HS_AUTH_BASE_URL = process.env.VUE_APP_HS_AUTH_BASE_URL || "https://stage.hypermine.in/hsauth/";
+export const HS_NODE_BASE_URL = process.env.VUE_APP_HS_NODE_BASE_URL || "https://api.fyre.hypersign.id/core/";
+export const SUPERHERO_HS_AUTH_BASE_URL = process.env.VUE_APP_HS_AUTH_BASE_URL || "https://api.fyre.hypersign.id/hsauth/";
 export const SUPERHERO_HS_AUTH_CREDENTIAL_ISSUE_API = "hs/api/v2/register"; // change supero to hypersign later
 export const HS_AUTH_DID_URL = SUPERHERO_HS_AUTH_BASE_URL + "hs/api/v2/authdid"
 export const WALLET_TYPE = process.env.VUE_APP_HS_WALLET_TYPE || "web"
