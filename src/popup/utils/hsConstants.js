6f0e0616512b50de1fe93114346698a8dc79abef
vishwas1 <vishwasanand.cse@gmail.com>
2022-09-26 09:25:35 +0530

    updated authserver path

--- src/popup/utils/hsConstants.js
+++ src/popup/utils/hsConstants.js
@@ -1,4 +1,4 @@
-export const SUPERHERO_HS_AUTH_BASE_URL = process.env.VUE_APP_HS_AUTH_BASE_URL || "https://jagrat.hypersign.id/authserver/";
+export const SUPERHERO_HS_AUTH_BASE_URL = process.env.VUE_APP_HS_AUTH_BASE_URL || "https://stage.hypermine.in/authserver/";
 export const SUPERHERO_HS_AUTH_CREDENTIAL_ISSUE_API = "hs/api/v2/register"; // change supero to hypersign later
 export const HS_AUTH_DID_URL = SUPERHERO_HS_AUTH_BASE_URL + "hs/api/v2/authdid"
 
