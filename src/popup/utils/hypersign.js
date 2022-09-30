76c6e83a910500b6e39130ed922c92d74bd25171
vishwas1 <vishwasanand.cse@gmail.com>
2022-09-30 19:52:12 +0530

    updated the schemaUrl logic

--- src/popup/utils/hypersign.js
+++ src/popup/utils/hypersign.js
@@ -1,8 +1,8 @@
 export function getSchemaIdFromSchemaUrl(schemaUrl) {
     if (!schemaUrl) throw new Error('SchemaUrl must be passed')
     const schUrl = new URL(schemaUrl)
-    let path = schUrl.pathname;
-    const schemaIDWithColon = path.substr('/hypersign-protocol/hidnode/ssi/schema/'.length, path.length - 1)
-    const schemaID = schemaIDWithColon.substring(0, schemaIDWithColon.length - 1)
+    const t = schUrl.pathname.split('/hypersign-protocol/hidnode/ssi/schema/');
+    const schemaID = schemaUrl.endsWith(':') ? t[1].substring(0, t[1].length - 1)  : t[1];
     return schemaID
 }
+
